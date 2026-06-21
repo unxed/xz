59a87df8957040f21b95aaf69910c6e36b30d65c
Ivan Sorokin <unxed@mail.ru>
2026-06-21 19:30:37 +0200

    Better benchmark

--- benchmark_test.go
+++ benchmark_test.go
@@ -1,6 +1,7 @@
 package xz_test
 
 import (
+    "sync"
 	"bytes"
 	"io"
 	"math/rand"
@@ -10,25 +11,33 @@ import (
 	"github.com/ulikunitz/xz/internal/randtxt"
 )
 
-func BenchmarkDecompressionSpeed(b *testing.B) {
-	// Подготовка данных (сжатие) происходит до старта таймера
-	const size = 5 * 1024 * 1024
+var (
+	benchDataOnce sync.Once
+	benchCompressedData []byte
+	benchOriginalSize int64
+)
+
+func prepareBenchData() {
+	const size = 20 * 1024 * 1024
 	r := io.LimitReader(randtxt.NewReader(rand.NewSource(42)), size)
-	originalData, _ := io.ReadAll(r)
-
-	var compressedBuf bytes.Buffer
-	w, _ := xz.NewWriter(&compressedBuf)
-	w.Write(originalData)
+	data, _ := io.ReadAll(r)
+	benchOriginalSize = int64(len(data))
+	var buf bytes.Buffer
+	w, _ := xz.NewWriter(&buf)
+	w.Write(data)
 	w.Close()
-	compressedData := compressedBuf.Bytes()
+	benchCompressedData = buf.Bytes()
+}
 
-	b.SetBytes(int64(len(originalData)))
+func BenchmarkDecompressionSpeed(b *testing.B) {
+	benchDataOnce.Do(prepareBenchData)
+
 	b.ReportAllocs()
-	b.ResetTimer() // Сбрасываем таймер перед основным циклом!
+	b.SetBytes(benchOriginalSize)
+	b.ResetTimer()
 
 	for i := 0; i < b.N; i++ {
-		inputReader := bytes.NewReader(compressedData)
-		reader, _ := xz.NewReader(inputReader)
-		io.Copy(io.Discard, reader) // Пишем в пустоту, чтобы мерить только LZMA
+		zr, _ := xz.NewReader(bytes.NewReader(benchCompressedData))
+		io.Copy(io.Discard, zr)
 	}
 }
